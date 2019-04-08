d880a50f27d79f58b4461c0f1c143af9c13e386c
mauroLonghi <mauro.longhi@globant.com>
2019-04-08 10:28:58 -0300

    comments Lucho

--- app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterPresenterTest.kt
+++ app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterPresenterTest.kt
@@ -1,12 +1,8 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation.mvp
 
 import com.puzzlebench.clean_marvel_kotlin.data.service.CharacterServicesImpl
-import com.puzzlebench.clean_marvel_kotlin.data.service.LoadCharacterLocalImpl
-import com.puzzlebench.clean_marvel_kotlin.data.service.StoreCharacterServiceImpl
 import com.puzzlebench.clean_marvel_kotlin.domain.model.Character
-import com.puzzlebench.clean_marvel_kotlin.domain.usecase.GetCharacterLoadUseCase
 import com.puzzlebench.clean_marvel_kotlin.domain.usecase.GetCharacterServiceUseCase
-import com.puzzlebench.clean_marvel_kotlin.domain.usecase.GetCharacterStoreUseCase
 import com.puzzlebench.clean_marvel_kotlin.mocks.factory.CharactersFactory
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.model.CharacterModel
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.presenter.CharacterPresenter
@@ -21,34 +17,26 @@ import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
+//TODO fix on second iteration
+// error: However, there was exactly 1 interaction with this mock:
 class CharacterPresenterTest {
 
     private var view = mock(CharacterView::class.java)
     private var model = mock(CharacterModel::class.java)
     private var characterServiceImp = mock(CharacterServicesImpl::class.java)
-
-    private var characterLoadImp = mock(LoadCharacterLocalImpl::class.java)
-    private var characterStoreImp = mock(StoreCharacterServiceImpl::class.java)
-
     private lateinit var characterPresenter: CharacterPresenter
-    private lateinit var characterServiceUseCase: GetCharacterServiceUseCase
-    private lateinit var characterLoadUseCase: GetCharacterLoadUseCase
-    private lateinit var characterStoreUseCase: GetCharacterStoreUseCase
+    private lateinit var getCharacterServiceUseCase: GetCharacterServiceUseCase
 
     @Before
     fun setUp() {
-
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> Schedulers.trampoline() }
-        characterServiceUseCase = GetCharacterServiceUseCase(characterServiceImp)
-        characterLoadUseCase = GetCharacterLoadUseCase(characterLoadImp)
-        characterStoreUseCase = GetCharacterStoreUseCase(characterStoreImp)
+        getCharacterServiceUseCase = GetCharacterServiceUseCase(characterServiceImp)
         characterPresenter = CharacterPresenter(view, model)
-
     }
 
     @Ignore
     fun reposeWithError() {
-        Mockito.`when`(characterServiceUseCase.invoke()).thenReturn(Observable.error(Exception("")))
+        Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(Observable.error(Exception("")))
         characterPresenter.init()
         verify(view).init()
         verify(characterServiceImp).getCaracters()
@@ -60,24 +48,21 @@ class CharacterPresenterTest {
     fun reposeWithItemToShow() {
         val itemsCharecters = CharactersFactory.getMockCharacter()
         val observable = Observable.just(itemsCharecters)
-        Mockito.`when`(characterServiceUseCase.invoke()).thenReturn(observable)
+        Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(observable)
         characterPresenter.init()
         verify(view).init()
         verify(characterServiceImp).getCaracters()
         verify(view).hideLoading()
         verify(view).showCharacters(itemsCharecters)
-
     }
 
     @Test
     fun reposeWithoutItemToShow() {
         val itemsCharacters = emptyList<Character>()
         val observable = Observable.just(itemsCharacters)
-        Mockito.`when`(characterServiceUseCase.invoke()).thenReturn(observable)
+        Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(observable)
         characterPresenter.init()
         verify(view).init()
         verify(characterServiceImp).getCaracters()
-
     }
-
 }
